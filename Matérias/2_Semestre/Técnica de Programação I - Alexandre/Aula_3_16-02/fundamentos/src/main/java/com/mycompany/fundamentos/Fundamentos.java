bc21b895b2664b1df74c5a186c17589fbf785511
joaop06 <joaoofficialpedro@gmail.com>
2023-02-16 22:23:28 -0300

    Tec Programação I - Inicialização Java / Primeiro Arquivo

---
+++ Matérias/2_Semestre/Técnica de Programação I - Alexandre/Aula_3_16-02/fundamentos/src/main/java/com/mycompany/fundamentos/Fundamentos.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.fundamentos;
+
+/**
+ *
+ * @author joaoo
+ */
+public class Fundamentos {
+
+    // Método padrão quando não há objetos criados
+    // void: seria o retorno do método / Os parâmetros da main, neste caso, são uma String[] que será alocada dentro da variável args.
+    public static void main(String[] args) { 
+        System.out.println("Hello World!");
+    }
+}
