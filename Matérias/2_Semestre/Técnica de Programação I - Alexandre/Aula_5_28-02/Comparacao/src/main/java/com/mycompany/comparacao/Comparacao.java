630312c5cf3d12c0877d37f5b5228e313f45ab26
joaop06 <joaoofficialpedro@gmail.com>
2023-02-28 22:23:28 -0300

    2) Tec Program - Teoria Operadores Java

--- Matérias/2_Semestre/Técnica de Programação I - Alexandre/Aula_5_28-02/Comparacao/src/main/java/com/mycompany/comparacao/Comparacao.java
+++ Matérias/2_Semestre/Técnica de Programação I - Alexandre/Aula_5_28-02/Comparacao/src/main/java/com/mycompany/comparacao/Comparacao.java
@@ -3,6 +3,7 @@
  */
 
 package com.mycompany.comparacao;
+import java.util.Scanner;
 
 /**
  *
@@ -11,6 +12,13 @@ package com.mycompany.comparacao;
 public class Comparacao {
 
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         System.out.println("Hello World!");
+        int num1, num2;
+        
+        System.out.print("Digite o primeiro número: ");
+        num1 = input.nextInt();
+        System.out.print("Digite o segundo número: ");
+        num2 = input.nextInt();
     }
 }
