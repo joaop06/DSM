fec27d8e81ebd3285c2a246bedc4486bade24fd0
ica87 <Ivan2079>
2023-03-26 15:58:05 -0300

    Att Trabalho Fausto

---
+++ 2_Semestre/Estrutura de Dados - Fausto/Tarefas/Trabalho T1 25-03/Bubblu_Sort.py
@@ -0,0 +1,41 @@
+passadas = comparacoes = trocas = 0
+
+def bubble_sort(empresas):
+
+    
+    global passadas, comparacoes, trocas
+    passadas = comparacoes =  trocas = 0
+
+
+    while True:
+        trocou = False
+        passadas+=1
+
+        for i in range(len(empresas)-1):
+            comps += 1
+
+            if empresas[1 + i] < empresas[i]:
+                empresas[1 + i], empresas[i] = empresas[i], empresas[1 + i]
+                trocou = True
+                trocas += 1
+        
+        if not trocou:
+            break
+
+
+import sys
+sys.dont_write_bytecode = True
+from dados.emp100mil import empresas
+from time import time
+
+
+tracemalloc.start()
+
+
+hora_ini = time()
+bubble_sort(empresas)
+hora_fim = time()
+mem_atual, mem_pico = tracemalloc.get_traced_memory()
+
+
+print(f'\n\n\nEmpresas Ordenadas: {empresas}\n\nTempo Gasto: {hora_fim - hora_ini}\n\nPico de Memória: {mem_pico / 1024 / 1024} MB\n\nComparações: {comps}, Trocas: {trocas}, Passadas: {passadas}.')
--- 2_Semestre/Estrutura de Dados - Fausto/ed-not-2023-1/README.md
+++
@@ -1,2 +0,0 @@
-# ed-not-2023-1
-Repositório da disciplina Estruturas de Dados, 2º semestre DSM noturno Fatec Franca 2023/1
