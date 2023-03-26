86aabc5b2d3ba7fb1333665baf79cef7d08c4c57
joaop06 <joaoofficialpedro@gmail.com>
2023-03-26 16:48:19 -0300

    Trabalho fausto

--- 2_Semestre/Estrutura de Dados - Fausto/Tarefas/Trabalho T1 25-03/Bubblu_Sort.py
+++ 2_Semestre/Estrutura de Dados - Fausto/Tarefas/Trabalho T1 25-03/Bubblu_Sort.py
@@ -1,41 +1,33 @@
 passadas = comparacoes = trocas = 0
-
 def bubble_sort(empresas):
-
-    
     global passadas, comparacoes, trocas
     passadas = comparacoes =  trocas = 0
-
 
     while True:
         trocou = False
         passadas+=1
-
         for i in range(len(empresas)-1):
-            comps += 1
+            comparacoes += 1
 
             if empresas[1 + i] < empresas[i]:
                 empresas[1 + i], empresas[i] = empresas[i], empresas[1 + i]
                 trocou = True
                 trocas += 1
-        
+
         if not trocou:
             break
-
-
+#####################################
 import sys
 sys.dont_write_bytecode = True
 from dados.emp100mil import empresas
 from time import time
-
+import tracemalloc
 
 tracemalloc.start()
-
 
 hora_ini = time()
 bubble_sort(empresas)
 hora_fim = time()
 mem_atual, mem_pico = tracemalloc.get_traced_memory()
 
-
-print(f'\n\n\nEmpresas Ordenadas: {empresas}\n\nTempo Gasto: {hora_fim - hora_ini}\n\nPico de Memória: {mem_pico / 1024 / 1024} MB\n\nComparações: {comps}, Trocas: {trocas}, Passadas: {passadas}.')
+print(f'\n\n\nTempo Gasto: {hora_fim - hora_ini}\n\nPico de Memória: {mem_pico / 1024 / 1024} MB\n\nComparações: {comparacoes}, Trocas: {trocas}, Passadas: {passadas}.')
