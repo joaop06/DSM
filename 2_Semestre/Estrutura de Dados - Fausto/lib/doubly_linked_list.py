85cd1d6a073e6a0663f8a8fc69419b7d6c7e0296
Fulano de Tal <joaoofficialpedro@gmail.com>
2023-05-18 20:38:27 -0300

    Início dos Grafos - Estrutura de Dados

--- 2_Semestre/Estrutura de Dados - Fausto/lib/doubly_linked_list.py
+++ 2_Semestre/Estrutura de Dados - Fausto/lib/doubly_linked_list.py
@@ -100,45 +100,56 @@ class DoublyLinkedList:
         # Incrementa a quantidade de nodos da lista
         self.__count += 1
 
-
-    ''' Método de atalho para inserção no início da lista '''
+    """ Método de atalho para inserção no início da lista """
     def insert_front(self, val):
         self.insert(0, val)
 
-
-    ''' Método de atalho para inserção no final da lista '''
+    """ Método de atalho para inserção no final da lista """
     def insert_back(self, val):
         self.insert(self.get_count(), val)
 
-
-    ''' Método que remove o nodo da posição especificada '''
+    """ Método que remove o nodo da posição especificada """
     def remove(self, pos):
 
         # 1º caso: lista vazia ou posição fora dos limites
-        if self.get_count() == 0 or pos < 0 or pos > self.get_count() -1:
+        if self.get_count() == 0 or pos < 0 or pos > self.get_count() - 1:
             raise Exception("ERRO: posição inválida para remoção.")
         
-        # 2º caso: remoção do iníicio da lista
+        # 2º caso: remoção do início da lista
         if pos == 0:
             # Será removido o primeiro nodo da lista
             removed = self.__head
             # O novo __head passa a ser o sucessor do nodo removido
             self.__head = removed.next
-            # Se o novo __head for um nodo válido, ele não pode ter um antecessor
+            # Se o novo __head for um nodo válido, ele não pode ter um
+            # antecessor
             if self.__head is not None: self.__head.prev = None
-            # Em caso de remoção do único nodo restante da lista, __tail precesa passar a valer None também
+            # Em caso de remoção do único nodo restante da lista, __tail
+            # precisa passar a valer None também
             if self.get_count() == 1: self.__tail = None
-        
+
         # 3º caso: remoção do final da lista
-        elif pos == self.get_count() -1:
+        elif pos == self.get_count() - 1:
             # Será removido o último nodo da lista
             removed = self.__tail
             # O novo __tail passa a ser o antecessor do nodo removido
             self.__tail = removed.prev
-            # Se o novo __tail for um nodo válido, ele não pode ter um sucessor
+            # Se o novo __tail for um nodo válido, ele não pode ter um
+            # sucessor
             if self.__tail is not None: self.__tail.next = None
-            # Em caso de remoção do único nodo restante da lista, __head precisa passar a valer None também
+            # Em caso de remoção do único nodo restante da lista, __head
+            # precisa passar a valer None também
             if self.get_count() == 1: self.__head = None
+
+        # 4º caso: remoção em posição intermediária
+        else:
+            removed = self.__find_node(pos)
+            before = removed.prev   # Nodo anterior ao que está sendo removido
+            after = removed.next    # Nodo posterior ao que está sendo removido
+            # O nodo anterior passa a apontar, à frente, para o nodo posterior
+            before.next = after
+            # O nodo posterior passa a apontar, para trás, para o nodo anterior
+            after.prev = before
 
         # Decrementa a quantidade de itens da lista
         self.__count -= 1
@@ -146,12 +157,53 @@ class DoublyLinkedList:
         # Retorna a informação do usuário armazenada no nodo removido
         return removed.data
 
+    """ Método de atalho para remoção do primeiro nodo """
+    def remove_front(self):
+        return self.remove(0)
+    
+    """ Método de atalho para remoção do último nodo """
+    def remove_back(self):
+        return self.remove(self.get_count() - 1)
+    
+    """
+        Método que retorna o valor armazenado na posição especificada,
+        sem removê-lo
+    """
+    def peek(self, pos):
+        # 1º caso: lista vazia ou posição fora dos limites
+        if self.get_count() == 0 or pos < 0 or pos >= self.get_count():
+            raise Exception("ERRO: posição inválida para consulta")
+        
+        node = self.__find_node(pos)
+        return node.data
+    
+    """ Método de atalho para consultar o primeiro nodo """
+    def peek_front(self):
+        return self.peek(0)
+    
+    """ Método de atalho para consultar o último nodo """
+    def peek_back(self):
+        return self.peek(self.get_count() - 1)
+    
+    """ 
+        Método que procura um nodo por seu valor
+        Retorna a posição do nodo, se o encontrar, ou -1, caso contrário
+    """
+    def find(self, val):
+        node = self.__head
+        for pos in range(0, self.get_count()):
+            if node.data == val: return pos     # Encontrou o valor
+            node = node.next
+        return -1       # Valor não encontrado
 
-    ''' Método que exibe uma representação da lista como String '''
+    """
+        Método que exibe uma representação da lista como string
+    """
     def __str__(self):
         if self.get_count() == 0: return "*** [ LISTA VAZIA ] ***\n\n"
         else:
-            repr = "LISTING {}"
+            repr = f"LISTING {self.get_count()} ITEM(S)\n"
+            repr += ('=' * 50) + "\n"
             node = self.__head
             for pos in range(self.get_count()):
                 repr += f"NODE #{pos}, address: {hex(id(node))}\n"
