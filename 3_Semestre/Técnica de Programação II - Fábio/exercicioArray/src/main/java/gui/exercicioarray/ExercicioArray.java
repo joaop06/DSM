2048643a71ce1a1bebec71b0587d447ba3618c44
Fulano de Tal <joaoofficialpedro@gmail.com>
2023-08-19 20:43:16 -0300

    Feat: Exercício Técnica de Programação II

---
+++ 3_Semestre/Técnica de Programação II - Fábio/exercicioArray/src/main/java/gui/exercicioarray/ExercicioArray.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package gui.exercicioarray;
+import java.util.Random;
+
+/**
+ *
+ * @author joaoo
+ */
+public class ExercicioArray {
+
+    public static void main(String[] args) {
+        
+    }
+    
+    public void randomVet(Integer[] Teste){
+        Random random = new Random();
+        int min = 1;
+        int max = 1000;
+        int randomNumber = random.nextInt(max - min) + min;
+        int[] vetNumbers = new int[10];
+        
+        for (int i = 0; i <= randomNumber; i++){
+            
+        }
+    }
+}
