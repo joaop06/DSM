3edb0f88aa3d4f4eb4d1fd6098f953be922a7bae
Fulano de Tal <joaoofficialpedro@gmail.com>
2023-08-26 17:34:53 -0300

    Orientação a Objeto Java

---
+++ 3_Semestre/Técnica de Programação II - Fábio/Animal/src/main/java/gui/animal/Animal.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package gui.animal;
+
+/**
+ *
+ * @author joaoo
+ */
+public class Animal {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
