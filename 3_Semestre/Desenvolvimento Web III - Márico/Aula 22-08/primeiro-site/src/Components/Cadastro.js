64c5c890504644bdb623eb1c55296bcccd63d3f6
joaop06 <joaoofficialpedro@gmail.com>
2023-09-07 10:54:16 -0300

    Feat: Tarefa 2 Dev Web III

--- 3_Semestre/Desenvolvimento Web III - Márico/Aula 22-08/primeiro-site/src/Components/Cadastro.js
+++ 3_Semestre/Desenvolvimento Web III - Márico/Aula 22-08/primeiro-site/src/Components/Cadastro.js
@@ -1,27 +1,47 @@
-import { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+
 function Cadastro() {
-    const [input, setInput] = useState('')
+    const [input, setInput] = useState('');
     const [tarefas, setTarefas] = useState([
         "Pagar conta de luz",
         "Estudar programação",
         "Enviar a tarefa"
-    ])
+    ]);
+    const [namesUser, setNamesUser] = useState([]);
+    const [selectedColor, setSelectedColor] = useState(); // Estado para rastrear a cor selecionada
 
-    function handleRegistro(e) {
-        e.preventDefault()
+    const tarefasStorage = localStorage.getItem('@tarefa');
+    const nameUser = localStorage.getItem('nome');
+    let name;
 
-        setTarefas([...tarefas, input])
-        setInput('')
-    }
+    useEffect(() => {
+        if (tarefasStorage) {
+            setTarefas(JSON.parse(tarefasStorage));
+        }
+        if (nameUser !== '') {
+            name = prompt('Digite seu nome:');
+            setNamesUser(name);
+        }
+    }, []);
 
+    useEffect(() => {
+        localStorage.setItem('@tarefa', JSON.stringify(tarefas));
+        localStorage.setItem('nome', JSON.stringify(namesUser));
+    }, [tarefas, namesUser]);
+
+    // Função para lidar com a seleção de cor
+    const handleColorChange = (color) => {
+        setSelectedColor(color);
+    };
 
     return (
-        <div>
-            <h1>Cadastro de Tarefas</h1>
+        <div style={{ backgroundColor: selectedColor, minHeight: '100vh', transition: 'background-color 0.5s' }}>
+            <h1>Olá, {localStorage.getItem('nome')}! Sua lista de tarefas:</h1>
 
             <form onSubmit={handleRegistro}>
                 <label>Nome da Tarefa: </label><br />
-                <input placeholder='Digite uma Tarefa'
+                <input
+                    placeholder='Digite uma Tarefa'
                     value={input}
                     onChange={(e) => setInput(e.target.value)}
                 /><br />
@@ -30,12 +50,51 @@ function Cadastro() {
             </form>
             <br /><br />
 
+            {/* Adicione Radio Buttons para escolher uma cor */}
+            <div>
+                <label>
+                    <input
+                        type="radio"
+                        value="blueviolet"
+                        checked={selectedColor === "blueviolet"}
+                        onChange={() => handleColorChange("blueviolet")}
+                    />
+                    blueviolet
+                </label>
+                <label>
+                    <input
+                        type="radio"
+                        value="cadetblue"
+                        checked={selectedColor === "cadetblue"}
+                        onChange={() => handleColorChange("cadetblue")}
+                    />
+                    cadetblue
+                </label>
+                <label>
+                    <input
+                        type="radio"
+                        value="lightcoral"
+                        checked={selectedColor === "lightcoral"}
+                        onChange={() => handleColorChange("lightcoral")}
+                    />
+                    lightcoral
+                </label>
+            </div>
+            <br /><br />
+
             <ul>
-                {tarefas.map(tarefa => (
-                    <li key={tarefa}>{tarefa}</li>
+                {tarefas.map((tarefa, index) => (
+                    <li key={index}>{tarefa}</li>
                 ))}
             </ul>
         </div>
     );
+
+    function handleRegistro(e) {
+        e.preventDefault();
+        setTarefas([...tarefas, input]);
+        setInput('');
+    }
 }
-export default Cadastro
+
+export default Cadastro;
