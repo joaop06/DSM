a065a45ef3624c50a8b0f36d257f170ec892b2db
joaop06 <joaoofficialpedro@gmail.com>
2023-09-11 22:17:28 -0300

    Atualização arquivos

--- 3_Semestre/Banco de Dados Não Relacional - Fausto/Projeto Fausto/back-end/src/routes/curso.js
+++ 3_Semestre/Banco de Dados Não Relacional - Fausto/Projeto Fausto/back-end/src/routes/curso.js
@@ -1,8 +1,8 @@
 import { Router } from 'express'
-import controller from '../controller/curso'
+import controller from '../controllers/curso.js'
 
 const router = new Router()
 
-router.post('/curso', controller.create)
+router.post('/', controller.create)
 
 export default router
